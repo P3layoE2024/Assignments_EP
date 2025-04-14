ac503eeece369299f5f7be99b196cbe4f14d8559
Edgar Pelayo Jr <158336399+P3layoE2024@users.noreply.github.com>
2025-04-14 21:19:04 +0000

    Task 5 Complete

---
+++ attendance_4_14_25/task_5/target/debug/deps/task_5-137b24246ce37b05.d
@@ -0,0 +1,5 @@
+/workspaces/Assignments_EP/attendance_4_14_25/task_5/target/debug/deps/task_5-137b24246ce37b05: src/main.rs
+
+/workspaces/Assignments_EP/attendance_4_14_25/task_5/target/debug/deps/task_5-137b24246ce37b05.d: src/main.rs
+
+src/main.rs:
---
+++ attendance_4_14_25/task_5/target/debug/task_5.d
@@ -0,0 +1,1 @@
+/workspaces/Assignments_EP/attendance_4_14_25/task_5/target/debug/task_5: /workspaces/Assignments_EP/attendance_4_14_25/task_5/src/main.rs
